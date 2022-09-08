903a770056354198d376722c9c44222abcf07186
mounstroso30o <Carol.Sivila@fundacion-jala.org>
2022-09-07 23:47:37 -0400

    Task : Print score and kill counter
    Added speed reset function in game class, kill counter and score in show_menu method

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,3 +1,4 @@
+from tkinter.font import Font
 import pygame
 
 from dino_runner.components.dinosaur import Dinosaur
@@ -23,15 +24,18 @@ class Game:
         self.running =  False
         self.score = 0
         self.death_count = 0
+        self.last_score= 0
 
     def execute(self):
         self.running = True
         while self.running:
             if not self.playing:
                 self.show_menu()
+                self.reset_game()
 
         pygame.display.quit()
         pygame.quit()
+
     def run(self):
         # Game loop: events - update - draw
         self.playing = True
@@ -40,14 +44,13 @@ class Game:
             self.events()
             self.update()
             self.draw()
-        pygame.quit()
-
+            
     def events(self):
         for event in pygame.event.get(): # nos devuelve una lista de eventos 
             if event.type == pygame.QUIT: #quit es de tipo entero y es una constante q ua no se este jugando
                 self.playing = False 
                 self.running = False
-
+            
     def update(self):
         self.update_score()
         user_input = pygame.key.get_pressed()
@@ -58,7 +61,7 @@ class Game:
         self.score += 1
         if self.score % 100 == 0:
             self.game_speed += 5 
-
+        
     def draw(self):
         self.clock.tick(FPS) # numero de actualizaciones por segundo 
         self.screen.fill((255, 255, 255)) #screen es la ventana fill es para el color 
@@ -90,9 +93,11 @@ class Game:
             if event.type == pygame.QUIT:
                 self.playing = False
                 self.running = False
+                
             elif event.type == pygame.KEYDOWN:
                 self.run()
     def show_menu(self):
+        
         self.screen.fill((255, 255, 255))
         half_screen_height = SCREEN_HEIGHT //2
         half_screen_width = SCREEN_WIDTH //2
@@ -104,9 +109,23 @@ class Game:
             text_rect.center = (half_screen_width, half_screen_height)
             self.screen.blit(text, text_rect)#####mostrar numero de muertes 
         else:
-            pass
+            font = pygame.font.Font(FONT_STYLE, 30)
+            text = font.render("You are died", True, (0, 0, 0))
+            text_rect = text.get_rect()
+            text_rect.center = (half_screen_width, half_screen_height)
+            self.screen.blit(text, text_rect)
+            
+            text = text = font.render("Score: "+str(self.last_score), True, (0, 0, 0))
+            text_rect.center = (half_screen_width, half_screen_height+50)
+            self.screen.blit(text, text_rect)
+
+            text = text = font.render("Number of deaths: "+str(self.death_count), True, (0, 0, 0))
+            text_rect.center = (half_screen_width, half_screen_height+100)
+            self.screen.blit(text, text_rect)
 
         self.screen.blit(ICON,(half_screen_width - 20, half_screen_height - 140))
 
         pygame.display.update() #para que dibuje lo que quiero dibujar
         self.handle_events_on_menu()
+    def reset_game(self):
+        self.game_speed=20
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -3,8 +3,7 @@ import random
 import pygame
 
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.components.obstacles.large_cactus import LargeCactus
-from dino_runner.components.obstacles.birds import Birds
+from dino_runner.components.obstacles.bird import Bird
 from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD
 
 class ObstacleManager:
@@ -13,20 +12,20 @@ class ObstacleManager:
         
     def update(self, game):
         if len(self.obstacles) == 0:
-            self.type = random.randint(0, 2)
+            self.type = random.randint(0, 1)
             if self.type == 0:
-                self.obstacles.append(Cactus(SMALL_CACTUS))
+                self.obstacles.append(Cactus(LARGE_CACTUS,SMALL_CACTUS))
             elif self.type == 1:
-                self.obstacles.append(LargeCactus(LARGE_CACTUS))
-            elif self.type == 2:
-                self.obstacles.append(Birds(BIRD))
+                self.obstacles.append(Bird(BIRD))
         
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(500)
+                game.last_score = game.score
                 game.playing = False
                 game.death_count += 1 
+                game.score = 0
                 break
         
     def draw(self, screen):
@@ -36,4 +35,3 @@ class ObstacleManager:
     def reset_obstacles(self):
         self.obstacles = []
 
-        
